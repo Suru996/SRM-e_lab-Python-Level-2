0e218f2859a900e161e9aeda4d5616cfa5f7d452
Surendra Pratap Dhamala <83567924+Suru996@users.noreply.github.com>
2021-07-23 09:13:22 +0545

    Delete Remove Vowel.py

--- Remove Vowel.py
+++
@@ -1,9 +0,0 @@
-a = input()
-b = len(a)
-c = ''
-for x in range(0,b):
-  if (a[x] == 'a' or a[x] == 'e' or a[x] == 'i' or a[x] == 'o' or a[x] == 'u'):
-    continue
-  else:
-    c += a[x]
-print(c)
